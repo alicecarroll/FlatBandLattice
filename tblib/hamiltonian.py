a0ca372df07f76340fa9a3f29784c8aa5df1f175
alicecarroll <t.tascheta@web.de>
2025-10-17 16:44:37 +0200

    3D plots, DOS

--- tblib/hamiltonian.py
+++ tblib/hamiltonian.py
@@ -1,14 +1,18 @@
 from . import lattice
 import numpy as np
 import scipy.constants as sc
+import matplotlib.pyplot as plt
 
 
 class Model:
     def __init__(self, **kwargs):
 
         self.N = kwargs.get('N', 2)
-        self.dim = self.N**2*4
         self.kind = kwargs.get('kind', 'DSL')
+        if self.kind == 'DSL':
+            self.dim = self.N**2*4
+        elif self.kind == 'dDSL':
+            self.dim = (self.N**2-self.N+1)*4
 
         self.t = kwargs.get('t', 1.0)
         self.mu = kwargs.get('mu', np.zeros(self.N))
@@ -22,12 +26,13 @@ class Model:
 
         if self.kind == 'DSL':
             lat = lattice.DiagonallyStripedLattice(N=self.N)
+            n = self.N**2
         if self.kind == 'dDSL':
             lat = lattice.dDiagonallyStripedLattice(N=self.N)
+            n = self.N**2-self.N+1
 
-        
-        mu_d = {i: [((i+j)%self.N, j) for j in range(self.N)] for i in range(self.N)} #associate each site to its group of stripes via the chemical potential index
-        
+        mu_d = {i: [site for site in lat.nn if site in [((i+j)%self.N, j) for j in range(self.N)]] for i in range(self.N)} #associate each site to its group of stripes via the chemical potential index
+
         c=0
         map_site={}
         map_idx={}
@@ -38,11 +43,9 @@ class Model:
                 c+=1
 
         # print(mu_d)
-        # print(map_idx)
 
-        d1 = self.N**2 #for one spin direction
-        H = np.zeros((d1, d1), dtype=complex)        
-        
+        H = np.zeros((n, n), dtype=complex)        
+
         def fact(i,j,kx,ky):
             site = map_site[j]
             nn = map_site[i]
@@ -61,7 +64,6 @@ class Model:
             hkp = np.zeros_like(H, dtype=complex)
             hkh = np.zeros_like(H, dtype=complex)
 
-            eps = 1e-15
             for site in lat.nn:
                 for nn in lat.nn[site]:
                     j=map_idx[site]
@@ -69,28 +71,21 @@ class Model:
                     hkp[i,j] = fact(i,j,kx, ky)
                     hkh[i,j] = -np.conjugate(fact(i,j,kx, ky))
 
-            if self.kind == 'DSL':
-                for os in range(d1):
-                    site = map_site[os]
+            for os in range(n):
+                site = map_site[os]
+                if site in lat.nn:
                     num = [key for key,val in mu_d.items() if site in val]
                     hkp[os,os] = self.mu[num[0]]
                     hkh[os,os] = -np.conjugate(self.mu[num[0]])
-            elif self.kind == 'dDSL':
-                for os in range(d1):
-                    site = map_site[os]
-                    if site in lat.nn:
-                        num = [key for key,val in mu_d.items() if site in val]
-                        hkp[os,os] = self.mu[num[0]]
-                        hkh[os,os] = -np.conjugate(self.mu[num[0]])
-
+            
             #hk[np.abs(hk) < eps] = 0
             A = np.zeros((self.dim,self.dim), dtype=complex)
-            A[:d1, :d1] = hkp
-            A[d1:2*d1, d1:2*d1] = hkp
-            A[d1*2:d1*3, d1*2:d1*3]=hkh
-            A[d1*3:, d1*3:]=hkh
+            A[:n, :n] = hkp
+            A[n:2*n, n:2*n] = hkp
+            A[n*2:n*3, n*2:n*3]=hkh
+            A[n*3:, n*3:]=hkh
 
-            return A
+            return A, hkp
 
         return Hk
 
@@ -103,7 +98,23 @@ class Model:
             eig = np.zeros((n, self.dim))
         
             for i in range(n):
-                e = np.linalg.eigvalsh(self.Hk(kx[i], ky[i])) #
+                e = np.linalg.eigvalsh(self.Hk(kx[i], ky[i])[0]) #
+
+                #e[np.abs(e)<eps]=0
+                eig[i]=np.sort(e)
+                
+            return eig.T
+
+    def solvParticleHam(self, kx, ky):
+            '''
+            solves hamiltonian for each pair of coordinates
+            '''
+            eps = 1e-15
+            n = np.shape(kx)[0]
+            eig = np.zeros((n, int(self.dim/4)))
+        
+            for i in range(n):
+                e = np.linalg.eigvalsh(self.Hk(kx[i], ky[i])[1]) #
 
                 #e[np.abs(e)<eps]=0
                 eig[i]=np.sort(e)
@@ -111,18 +122,74 @@ class Model:
             return eig.T
     
     def Es(self, k):
-        #E=np.array([[],[],[]])
+        '''
+        returns an array of the energies for each k-point in the BZ 
+        for the normal state particle Hamiltonian 
+        Array should have dimension of (N_bands,N_kpoints**2)
+        
+        '''
         l = np.shape(k)[0]
         a1 = np.ones(l)
-        E=np.empty((self.dim, l))
+        E=np.empty((int(self.dim/4), l))
         eps = 1e-15
 
         for i in k:
-            Erow = self.solvHam(i*a1, k)
+            Erow = self.solvParticleHam(i*a1, k)
             E = np.concatenate((E, Erow), axis=1)
             E[np.abs(E)<eps]=0
         return E
+    
+    def plot_particle_bands(self,k):
 
+        k1 = np.ones(100)
+        k0 = np.zeros(100)
+        path = np.concatenate((k, k, k*np.sqrt(2)))
+        kx = np.concatenate((k,k[-1]*k1, k[::-1]))
+        ky = np.concatenate((k0, k, k[::-1]))
+
+        p = [i for i in range(np.shape(path)[0])]
+        np.shape(p)
+        energies = self.solvParticleHam(kx, ky)
+
+        emax = np.amax(energies)
+        emax = emax+0.1*emax
+
+        plt.figure(figsize=(8,6))
+        plt.xlabel("$(k_x a,k_y a)$", size='x-large')
+        plt.ylabel("E", size='x-large')
+        plt.yticks(size='x-large')
+        plt.xticks(ticks= [0, 100, 200, 299], labels=[r"$\Gamma$",r"X",r"M", r"$\Gamma$"], size='x-large')
+
+        for i in energies:
+            plt.plot(p, i, color='black')
+
+        plt.vlines([0, 99, 199, 299], [-emax, -emax, -emax, -emax], [emax, emax, emax, emax], colors= 'grey', linestyles='--')
+        plt.show()
+        return
+    
+    def DOS(self, E, k, b=0, sig=5e-2):
+        ''' 
+        returns an array with the density of state values to each E
+
+        E is an array (against which DOS is plotted)
+        En is an array with the E-values for all k grid points of shape (3, ???) because we currently have 3 bands
+        sigma is the width of the gauss function
+        '''
+        def Gauss(E, En, sig):
+            return np.exp(-(E-En)**2/(2*sig**2))
+            
+        En = self.Es(k)
+        l = np.shape(En)[1]
+        arr1 = np.ones(l)
+        s1 = 0
+        res = np.ones(np.shape(E)[0])
+        c=0
+        for j in E:
+            s1 += np.sum(Gauss(j*arr1,En[b], sig))
+            res[c] = s1
+            s1=0
+            c+=1
+        return res
  
 
 
