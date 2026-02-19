338695013c54d330795193640a67568f8e8f04a0
Anna M. T. <t.tascheta@web.de>
2026-02-19 14:32:48 +0100

    i

--- tblib/superfluid.py
+++ tblib/superfluid.py
@@ -216,7 +216,7 @@ def SFWconv(s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t, nu, T, U, ns, 
             dH_ny = hamiltonian_opti.H_kin(H.copy(), kx, ky, dny[0], dny[1], s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t)
 
             H_up = hamiltonian_opti.H_kin(H.copy(), kx, ky, 0, 0, s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t)[:n,:n]
-            H_down = -hamiltonian_opti.H_kin(H.copy(), kx, ky, 0, 0, s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t)[n:,n:]
+            H_down = hamiltonian_opti.H_kin(H.copy(), kx, ky, 0, 0, s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t)[n:,n:]
             
             evals[:] = eval_arr[counter]
             Evec[:] = evec_arr[counter]
@@ -237,7 +237,7 @@ def SFWconv(s_idx, n_idx, sx, sy, nx, ny, R_ptr, R_flat, n, N, t, nu, T, U, ns, 
             for i in range(2*n):
                 s_array[i]= np.linalg.solve(m_mat.T, Evec[:,i])
             if counter <4:
-                print(s_array)
+                print(m_mat.T, '\n\n', Evec.T)
 
             for ei,E in enumerate(evals):
                 nE[ei] = fermidirac(E,T,o=0)
